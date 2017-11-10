fc246d03d34848a3f04bd0172a524f3817619ff9
Martin Bing <bing.martin.photography@gmail.com>
2017-11-10 22:47:09 +0100

    Add prototype and expected scope vars

--- js/clock.js
+++ js/clock.js
@@ -3,24 +3,8 @@
     function Clock (elementId, offset = 0, label = 'UTC') {
         let d = new Date();
         this.offset = (offset + d.getTimezoneOffset()) * 60 * 1000;
-        this.formatDigits = function (time) {
-                if (time < 10) {
-                    time = `0${time}`;
-                }
-                return time;
-            };
-        this.update = function () {
-                let date = new Date();
-                date = new Date(this.offset + date.getTime());
-                let time = {
-                    hours: date.getHours(),
-                    minutes: date.getMinutes(),
-                    seconds: date.getSeconds(),
-                };
-                let clockEl = document.getElementById(elementId);
-
-                clockEl.innerHTML = `${this.formatDigits(time.hours)}:${this.formatDigits(time.minutes)}:${this.formatDigits(time.seconds)} ${label}`;
-            };
+        this.elementId = elementId;
+        this.label = label;
 
         this.update();
         var that = this;
@@ -29,7 +13,25 @@
         }, 1000 );
     }
 
+    Clock.prototype.formatDigits = function (time) {
+        if (time < 10) {
+            time = `0${time}`;
+        }
+        return time;
+    };
 
+    Clock.prototype.update = function () {
+        let date = new Date();
+        date = new Date(this.offset + date.getTime());
+        let time = {
+            hours: date.getHours(),
+            minutes: date.getMinutes(),
+            seconds: date.getSeconds(),
+        };
+        let clockEl = document.getElementById(this.elementId);
+
+        clockEl.innerHTML = `${this.formatDigits(time.hours)}:${this.formatDigits(time.minutes)}:${this.formatDigits(time.seconds)} ${this.label}`;
+    };
 
     function onReady () {
         let clock1 = new Clock('clock');
